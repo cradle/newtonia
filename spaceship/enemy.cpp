cea4307885965ad01d235fae91fcdce50770527a
Glenn Murray <cradle@gmail.com>
2008-12-02 12:04:54 +1100

    Kill. Gloat. Find new target.

--- spaceship/enemy.cpp
+++ spaceship/enemy.cpp
@@ -22,6 +22,10 @@ Enemy::Enemy(float x, float y, std::vector<Ship*>* targets, int difficulty) : Ca
 }
 
 void Enemy::lock_nearest_target() {
+  if(target && !target->is_alive()) {
+    target = NULL;
+  }
+
   for(unsigned int i = 0; i < targets->size(); i++) {
     if((*targets)[i]->is_alive()) {
       if(target == NULL || (*targets)[i]->position.distance_to(position) < target->position.distance_to(position)) {
@@ -39,22 +43,23 @@ void Enemy::step(float delta) {
   Car::step(delta);
   if(is_alive()) {
     velocity = velocity - velocity * 0.0005 * delta;
-
-    // bool close = (target->position - position).magnitude() < 100.0;
-    time_until_next_shot -= delta;
-    while(time_until_next_shot <= 0) {
-      fire_shot();
-      time_until_next_shot += time_between_shots;
-    }
     
     time_until_next_lock -= delta;
-    while(time_until_next_lock <= 0) {
+    if(time_until_next_lock <= 0) {
       lock_nearest_target();
       time_until_next_lock += time_between_locks;
     }
     
     if(target) {
       if(target->is_alive()) {
+	  
+		// bool close = (target->position - position).magnitude() < 100.0;
+		time_until_next_shot -= delta;
+		while(time_until_next_shot <= 0) {
+		  fire_shot();
+		  time_until_next_shot += time_between_shots;
+		}
+		
         float angle = (heading() - (position.closest_to(target->position) - target->position).normalized().direction());
         if (angle >= 0 && angle < 180 || angle >= -360 && angle < -180) {
           rotate_left(true);
@@ -62,6 +67,9 @@ void Enemy::step(float delta) {
           rotate_right(true);
         }
       } else {
+		target = NULL;
+        time_until_next_lock = 2000.0;
+		time_until_next_shot = time_until_next_lock + time_between_shots;
         rotate_right(false);
       }
     }
