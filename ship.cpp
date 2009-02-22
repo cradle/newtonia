3ccac056c86920eea20ceb0fd5f4f3c4f95aaeb8
Glenn Murray <cradle@gmail.com>
2009-02-23 01:52:23 +1100

    Unified disabled heating for now.

--- ship.cpp
+++ ship.cpp
@@ -29,23 +29,20 @@ void Ship::init(bool no_friction) {
   max_temperature = 100.0;
   critical_temperature = max_temperature * 0.80;
   explode_temperature = max_temperature * 1.2;
+  heat_rate = 0.000;
+  retro_heat_rate = heat_rate * -reverse_force / thrust_force;
+  cool_rate = retro_heat_rate * 0.9;
 
   if(no_friction) {
     friction = 0;
     reverse_force = -0.01;
     thrust_force = 0.03;
     rotation_force = 0.3;
-    heat_rate = 0.000;
-    retro_heat_rate = heat_rate * -reverse_force / thrust_force;
-    cool_rate = retro_heat_rate * 0.85;
   } else {
     friction = 0.001;
     reverse_force = -0.05;
     thrust_force = 0.09;
     rotation_force = 0.2;
-    heat_rate = 0.035;
-    retro_heat_rate = heat_rate * -reverse_force / thrust_force;
-    cool_rate = retro_heat_rate * 0.9;
   }
 
   reset();
